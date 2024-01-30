cbaaf8d8d457ac1c28cf81d347b5a286d2a2b60e
mlun <max-lunitsin@mail.ru>
2024-01-31 01:27:36 +0300

    add other puzzles

--- pypy_gen_sub.py
+++ pypy_gen_sub.py
@@ -18,12 +18,16 @@ gap_dir = "./gap_data/"
 
 
 ltypes = [
-    'cube_3/3/3',
-    # 'globe_3/33', 
-    # 'globe_8/25',
-    # 'globe_6/10',
+    'cube_2/2/2', 'cube_3/3/3', 'cube_4/4/4', 
+    # 'cube_5/5/5', 'cube_6/6/6', 'cube_7/7/7', 
+    # 'cube_8/8/8', 'cube_9/9/9', 'cube_10/10/10',
+    # 'cube_19/19/19', 
+    # 'cube_33/33/33', 
+    # 'wreath_6/6', 'wreath_7/7', 'wreath_12/12', 'wreath_21/21', 'wreath_33/33', 'wreath_100/100',
+    # 'globe_1/8', 'globe_1/16', 'globe_2/6', 'globe_3/4', 'globe_6/4', 
+    # 'globe_6/8', 'globe_6/10', 'globe_3/33', 'globe_8/25'
     ]
-sub = pd.read_csv('./submission_nxnxn.csv')
+sub = pd.read_csv('./sample_submission.csv')
 nretry = 1
 
 
